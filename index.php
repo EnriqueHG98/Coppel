408598efeb46f38d8d8752ed8f3c852dfd3b99da
EnriqueHG98 <131415213+EnriqueHG98@users.noreply.github.com>
2023-09-27 10:27:31 -0600

    Carga de archivos
    Upload de primeros archivos *index, styles*

---
+++ index.php
@@ -0,0 +1,31 @@
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Coppel - Hogar</title>
+    <link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!--Nav bar-->
+    <div class="navbar"></div>
+
+    <!--Content-->
+    <section class="content" id="first-section">
+        <div class="box-title">
+            <h1 class="category-title">Hogar y muebles</h1>
+        </div>
+        <div>
+            productos
+        </div>
+    </section>
+    <section class="content" id="second-section">
+        <div class="box-content">
+
+        </div>
+    </section>
+</body>
+
+
+</html>
