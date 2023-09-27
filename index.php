0fbfd40cd2e9553ba2a0da02af04e71f7e02ac33
EnriqueHG98 <131415213+EnriqueHG98@users.noreply.github.com>
2023-09-27 11:36:54 -0600

    Carrusel new files
    Inrtegraciondel carrusel y carga de archivos js y css para el mismo

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <title>Coppel - Hogar</title>
     <link href="style.css" rel="stylesheet">
+    <link href="slider.css" rel="stylesheet">
 </head>
 
 <body>
@@ -22,10 +23,44 @@
     </section>
     <section class="content" id="second-section">
         <div class="box-content">
+            <!--Slider-->
+            <div class="slider-container">
+                <div class="slide fade">
+                    <img class="slide-image" src="https://cdn2.coppel.com/images/emarketing/1stlevel/2023/septiembre/hogar/Sliders/Hogar&Muebles-desktop-septiembre1.jpg">
+                </div>
+
+                <div class="slide fade">
+                    <img class="slide-image" src="https://cdn2.coppel.com/images/emarketing/1stlevel/2023/septiembre/hogar/Sliders/Hogar_Muebles-desktop-septiembre1.jpg">
+                </div>
+
+                <div class="slide fade">
+                    <img class="slide-image" src="https://cdn2.coppel.com/images/emarketing/1stlevel/2023/septiembre/hogar/Sliders/Hogar_Muebles-desktop-septiembre2.jpg">
+                </div>
+
+                <!-- dots -->
+                <div class="dot-container">
+                    <span class="dot" onclick="currentSlide(1)"></span>
+                    <span class="dot" onclick="currentSlide(2)"></span>
+                    <span class="dot" onclick="currentSlide(3)"></span>
+                </div>
+            </div>
+
+
+            <div class="img1">
+                <img src="https://cdn2.coppel.com/images/emarketing/1stlevel/2023/septiembre/hogar/Bloques/nuevosBloques/Bloque1-desktop.jpg">
+            </div>
+            <div class="img2">
+                <img src="https://cdn2.coppel.com/images/emarketing/1stlevel/2023/septiembre/hogar/Bloques/Bloque2-desktop.jpg">
+            </div>
+
+
 
         </div>
     </section>
 </body>
 
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+<script src="slider.js"></script>
 
 </html>
